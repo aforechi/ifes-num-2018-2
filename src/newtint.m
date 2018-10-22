ee33491333e020f26fd390e3311493238963088c
aforechi <avelino.forechi@gmail.com>
2018-10-22 09:03:08 -0300

    newton

---
+++ src/newtint.m
@@ -0,0 +1,21 @@
+function yint = newtint(x, y, xx)
+n = length(x);
+b = zeros(n,n);
+
+% atribui as variáveis dependentes à primeira coluna de b.
+b(:,1) = y(:); % o (:) garante que o y é um vetor coluna.
+
+for j = 2:n
+    for i = i:n-j+1
+        b(i,j) = ( b(i+1,j-1) - b(i,j-1) ) / ( x(i+j-1) - x(i) );
+    end
+end
+
+% usa a tabela de diferenças divididas para interpolar
+xt = 1;
+yint = b(1, 1);
+
+for j = 1:n-1
+    xt = xt * (xx - x(j));
+    yint = yint + b(1, j+1) * xt;
+end
