052f94996c1e749f5762982902d32eb7b61581e3
Avelino Forechi <avelino@Avelinos-MacBook-Pro.local>
2019-10-22 12:43:39 -0300

    add time and fixed pure pursuit

--- src/trabalho/@purepursuit/update.m
+++ src/trabalho/@purepursuit/update.m
@@ -4,18 +4,20 @@ function self = update(obj, x)
     end
     self = obj;
     
-    waypoints = waypoints_wrt_car(obj.waypoints, x);
-    
-    i = get(obj.waypoints, 'current');
-    
-    coeffs = polyfit(waypoints(i:i+4,1), waypoints(i:i+4,2), 3);
-        
     velocity = x(4);
     
     if velocity > 0.001            
-        lookahead_distance_x = self.lookahead_distance * velocity;          
-        cte = polyval(coeffs, lookahead_distance_x);            
-        self.steering = atan((2 * self.baseline * cte / lookahead_distance_x) / (self.gain * velocity));
+        i = get(obj.waypoints, 'current');
+
+        waypoints = waypoints_wrt_car(obj.waypoints, x);
+
+        coeffs = polyfit(waypoints(i:i+4,1), waypoints(i:i+4,2), 1);
+        
+        lookahead_distance_x = self.lookahead_distance + self.gain * velocity;          
+        
+        cte = polyval(coeffs, lookahead_distance_x);
+        
+        self.steering = atan(2 * self.baseline * cte / (lookahead_distance_x ^ 2));
     else
         self.steering = 0.0;
     end
